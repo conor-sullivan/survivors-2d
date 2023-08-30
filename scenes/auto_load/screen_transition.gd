616935e7476309ab4138ec329c1fa5de24fd485e
conorsullivan182@gmail.com <conorsullivan182@gmail.com>
2023-08-30 11:16:25 -0400

    Scene transition and animation.

---
+++ scenes/auto_load/screen_transition.gd
@@ -0,0 +1,19 @@
+extends CanvasLayer
+
+signal transitioned_halfway
+
+@onready var animation_player = $AnimationPlayer
+
+var skip_emit = false
+
+
+func transition():
+	$ColorRect.visible = true
+	animation_player.play("default")
+	await animation_player.animation_finished
+	transitioned_halfway.emit()
+	animation_player.play_backwards("default")
+	await animation_player.animation_finished
+	$ColorRect.visible = false
+
+
